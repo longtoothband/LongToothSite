efa5b3d7e21df829e7386be9b47b73278932632a
Danie K <longtoothwesternny@gmail.com>
2025-01-15 20:34:12 -0500

    Contact form
    Added contact form, and is working..still needs validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+emailjs.init("mq2m9pFC7sY4n-jMS")
+
+// SendMail function
+function SendMail(event){
+    event.preventDefault(); 
+
+    var params = {
+        from_name : document.getElementById("fullName").value.trim(),
+        email_id : document.getElementById("email_id").value.trim(),
+        message : document.getElementById("message").value.trim()
+    }
+    emailjs.send("service_k7hohbb","template_qmrxhsb", params).then(function(res){
+        alert("Success!" + res.status);
+        document.getElementById("contact-form").reset();
+    })
+    .catch(function (error) {
+        alert("Error: " + error.text);
+    });
+
+
+}
+
+// Add event listener to form
+document.getElementById("contact-form").addEventListener("submit", SendMail);
+
+
+
 document.querySelectorAll('.toggle-bio').forEach(button => {
     button.addEventListener('click', () => {
         const bioContainer = button.previousElementSibling;
@@ -15,3 +42,4 @@ document.querySelectorAll('.toggle-bio').forEach(button => {
         }
     });
 });
+
